fd2a41d741699314bb1bc78b37659c217de4888e
wb-jh356560 <wb-jh356560@alibaba-inc.com>
2019-06-26 16:18:18 +0800

    简单的node项目用来上传图片

---
+++ requestHandlers.js
@@ -0,0 +1,85 @@
+﻿var exec = require("child_process").exec;
+var fs = require("fs");
+var querystring = require("querystring"); 
+var formidable = require("formidable");
+function start(response){
+    console.log("Request handler 'start' was called");
+
+    function sleep(milliSeconds){
+        var startTime = new Date().getTime();
+        var i =  0;
+        while(new Date().getTime()<startTime+milliSeconds){
+            console.log(i++);
+        };
+    }
+
+
+    // sleep(10000);
+    // return "hello start";
+    // var content = "empty";
+    // exec("find /",  
+    // { timeout: 10000, maxBuffer: 20000*1024 },
+    // function(error,stdout,stderr){
+    //     // content  = stdout ;
+    //     response.writeHead(200,{"Content-Type":"text/plain"});
+    //     response.write(stdout);
+    //     response.end();
+    // })
+    // return content; 
+
+    var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+    
+    response.writeHead(200, {"Content-Type": "text/html;charset = utf-8",});
+    response.write(body);
+    response.end();
+}
+
+exports.upload = (response,request)=>{
+    console.log("Request handler 'upload' was called.");
+    // return "hello upload";
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("You've sent: " + querystring.parse(postData).text);
+//   response.end();
+    var form = new formidable.IncomingForm();
+    form.uploadDir='tmp';
+    console.log("about to parse")
+    form.parse(request,function(error,fields,files){
+        
+        console.log("parsing done");
+        console.log("files",files);
+        fs.renameSync(files.upload.path,"./tmp/test.png");
+        response.writeHead(200,{"Content-Type":"text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show'/>");
+        response.end();
+    })
+}
+function show(response,request){
+    console.log("Request handler 'show' was called.");
+    fs.readFile('./tmp/test.png','binary',function(error,file){
+        if(error){
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        }else{
+            response.writeHead(200,{"Content-Type":"image/png"});
+            response.write(file,"binary");
+            response.end();
+        }
+    })
+}
+
+exports.start = start;
+exports.show = show;
---
+++ server.js
@@ -0,0 +1,71 @@
+﻿var http = require("http");
+var url =  require("url");
+var util = require("util");
+var formidable = require("formidable");
+
+
+function start(route,handle){
+    http.createServer(function(request,response){
+        // var postData = '';
+        var pathname = url.parse(request.url).pathname;
+
+        route(handle,pathname,response,request)
+
+
+
+        // console.log(pathname,"pathname" );
+        // console.log(request.url,"request.url" );
+
+        // request.setEncoding("utf8")
+
+        // // var content =  route(handle,pathname,response)
+
+        // // response.writeHead(200,{"Content-Type":"text/plain"})
+        // // response.write(content);
+        // // response.end();
+
+        // request.addListener("data",function(postDataChunk){
+        //     console.log("postData: ",postData);
+        //     console.log("postDataChunk: ",postDataChunk);
+
+        //     postData += postDataChunk;
+            
+        //     console.log("Received POST data chunk '"+
+        //     postDataChunk + "'.");
+        // })
+        // request.addListener("end",function(){
+        //     route(handle,pathname,response,postData);
+        // })
+
+        // route(handle,pathname,response)
+
+        // if(request.url ==="/upload"&&request.method.toLowerCase()=="post"){
+        //      // parse a file upload
+        //      console.log("执行")
+        //     var form = new formidable.IncomingForm();
+        //     form.parse(request,function(err,fields,files){
+        //         response.writeHead(200, {'content-type': 'text/plain'});
+        //         response.write('received upload:\n\n');
+        //         response.end(util.inspect({fields,files}))
+        //     });
+        //     return ;
+        // }
+
+
+          // show a file upload form
+        //   response.writeHead(200, {'content-type': 'text/html'});
+        //   response.end(
+        //         '<form action="/upload" enctype="multipart/form-data" '+
+        //         'method="post">'+
+        //         '<input type="text" name="title"><br>'+
+        //         '<input type="file" name="upload" multiple="multiple"><br>'+
+        //         '<input type="submit" value="Upload">'+
+        //         '</form>'
+        //     );
+    }).listen(8888);
+    
+    
+    console.log("server has started")
+}
+
+exports.start = start ;
